657cfef64a33bd9d66002d27cc9a6bb8302e9b5e
yuoto13 <159054430+yuoto13@users.noreply.github.com>
2024-05-03 22:10:36 +0700

    3LABS 2ZAD

--- 3lab/1.py
+++ 3lab/1.py
@@ -1,23 +1,11 @@
-numbers = []
+numbers = list(map(int, input("числа через пробел: ").split()))
 
-# Запрос чисел у пользователя
-print("Введите числа")
-while True:
-    num_input = input("Введите число:")
-    if num_input == "":
-        break
-    try:
-        num = float(num_input)
-        numbers.append(num)
-    except ValueError:
-        print("Некорректный ввод. Попробуйте еще раз.")
-
-# Поиск наибольшего числа с использованием цикла for
-if numbers:
-    largest = numbers[0]
-    for num in numbers:
-        if num > largest:
-            largest = num
-
-    # Вывод результата
-    print("Наибольшее число в списке:", largest)
+if not numbers:  # Проверяем, что список не пуст
+    print("пусто тут")
+else:
+    largest = numbers[0]  # Первый элемент списка
+    for number in numbers:
+        if number > largest:
+            largest = number
+    print("макс:", largest)
+    
