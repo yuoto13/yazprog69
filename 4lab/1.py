67d40f121bc71d29ec14f4855474e90cec24a442
yuoto13 <159054430+yuoto13@users.noreply.github.com>
2024-05-18 17:04:49 +0700

    исправил 4 лабу

--- 4lab/1.py
+++ 4lab/1.py
@@ -1,12 +1,13 @@
-#ввод чисел через пробел
+def qwerty(numbers_input):
+    # Разбиваем введенную строку на отдельные числа и преобразуем их в float, затем создаем список
+    numbers = list(map(float, numbers_input.split()))
+    
+    # Вычисляем среднее арифметическое списка чисел или выводим 0, если список пустой
+    average = sum(numbers) / len(numbers) if numbers else 0
+    
+    return average
+
+# Пример использования функции
 numbers_input = input("Введите числа через пробел: ")
-
-# Разбиваем введенную строку на отдельные числа и преобразуем их в float, затем создаем список
-numbers = list(map(float, numbers_input.split()))
-
-# Вычисляем среднее арифметическое списка чисел или выводим 0, если список пустой
-average = sum(numbers) / len(numbers) if numbers else 0
-
-# Выводим среднее арифметическое
+average = qwerty(numbers_input)
 print("Среднее арифметическое:", average)
- 
